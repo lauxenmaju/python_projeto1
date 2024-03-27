eb4e69d85fa4e0dc1353fec0f27f01348357b704
lauxenmaju <lauxen.maria@escola.pr.gov.br>
2024-03-27 10:39:00 -0300

    subir aula 01

---
+++ aula.py
@@ -0,0 +1,13 @@
+import os
+#print("olá mundo cruel")
+
+print(" O 3DS é a turma número 1 do sagrada")
+turma="2DS"
+pos=2
+
+print("O",turma,"é a turma número",pos,"do sagrada")
+
+print(type(turma))
+print(type(pos))
+
+os.system("")
